c8b62f6b3d04e23783aaf765e8c2fd55d8f9d6a2
Syrkery <e3075589k@gmail.com>
2025-05-10 00:32:57 +0300

    The form for adding a product and saving data to the "Products" table

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, g
+from flask import Flask, render_template, g, request, redirect, url_for
 import sqlite3
 
 app = Flask(__name__)
@@ -21,6 +21,18 @@ def index():
     db = get_db()
     products = db.execute("""SELECT id, name, price, stock FROM products""").fetchall()
     return render_template('index.html', products=products)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_product():
+    if request.method == 'POST':
+        name = request.form['name']
+        price = request.form['price']
+        stock = request.form['stock']
+        db = get_db()
+        db.execute("""INSERT INTO products (name, price, stock) VALUES (?, ?, ?)""", (name, price, stock))
+        db.commit()
+        return redirect(url_for('index'))
+    return render_template('add_product.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080, host='127.0.0.1')
