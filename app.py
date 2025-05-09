8a32ec7b12145e7b845719dd028bf0b64bd37094
Syrkery <e3075589k@gmail.com>
2025-05-09 18:56:41 +0300

    connected the database

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
 import sqlite3
 
 app = Flask(__name__)
+DATABASE = 'database.sqlite3'
 
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(DATABASE)
+        g.db.row_factory = sqlite3.Row
+    return g.db
+
+@app.teardown_appcontext
+def close_db(error):
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
 @app.route("/")
 def index():
+    db = get_db()
     return render_template("index.html")
 
 if __name__ == "__main__":
