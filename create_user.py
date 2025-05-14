94fd1b6d4b1ed1e0e7481399571a855685ea4d3a
Syrkery <e3075589k@gmail.com>
2025-05-15 00:41:53 +0300

    Update create_user.py

--- create_user.py
+++ create_user.py
@@ -1,15 +1,16 @@
 import sqlite3
 from werkzeug.security import generate_password_hash
 
-email = 'admin@example.com'
-password = '12345'
+username = 'admin'
+email = 'admin@ya.ru'
+password = 'admin'
 
 conn = sqlite3.connect('database.sqlite3')
 cursor = conn.cursor()
 
 hashed = generate_password_hash(password)
 
-cursor.execute("""INSERT INTO Users (email, password) VALUES (?, ?)""", (email, hashed))
+cursor.execute("""INSERT INTO Users (username, email, password) VALUES (?, ?, ?)""", (username ,email, hashed))
 conn.commit()
 conn.close()
 
